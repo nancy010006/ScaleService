f7651bd3597bb8069682af80ca156ec614529a3f
jhjian <jeffjian0612@gmail.com>
2018-04-15 23:34:13 +0800

    新增問卷選項

--- resources/views/site/scale.blade.php
+++ resources/views/site/scale.blade.php
@@ -7,10 +7,18 @@
 		console.log(ScaleData);
 		$.each(ScaleData.dimensions,function(index,val){
 			var Qnum = 1;
-			$('#questions').append('<strong>'+val.name+'</strong>');
+			// var dp = 1;
+			// console.log(dp);
+			// var create_page = document.createElement('div');
+			// create_page.setAttribute("id",'div'+dp);
+			// dp++;
+			$('#questions').append('<br><div class="alert alert-warning"><ul class="list-group"><strong>'+val.name+'</strong></ul></div>');
 			$.each(val.questions,function(qindex,qval){
 				// console.log(qval);
-				$('#questions').append('<p>'+(Qnum++)+'.'+qval.description+'</p>');
+				$('#questions').append('<li class="list-group-item"><p>'+(Qnum++)+'.'+qval.description+'</p></li>');
+				for (var i = 1; i <= ScaleData.level; i++) {
+					$('#questions').append(i+'<input type="radio" name="'+ScaleData.dimensions[1].name+'"id="'+ScaleData.dimensions[1].name+i+'">');
+				}
 			})
 		})
 	})
@@ -27,5 +35,7 @@
 </script>
 @endsection
 @section('content')
-<div id="questions"></div>
+<div id="questions" class="container"></div>
+@endsection
+@section('css')
 @endsection
