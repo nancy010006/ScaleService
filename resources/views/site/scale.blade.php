28efb928747a08c3d185a4e51859d2f338533d0c
jhjian <jeffjian0612@gmail.com>
2018-04-20 13:54:44 +0800

    新增問卷換頁功能

--- resources/views/site/scale.blade.php
+++ resources/views/site/scale.blade.php
@@ -2,25 +2,24 @@
 @section('js')
 <script type="text/javascript">
 	var ScaleData;
+	var pd = 1;
+	var i = 1;
 	$(document).ready(function(){
 		init();
-		console.log(ScaleData);
+				console.log(ScaleData);
 		$.each(ScaleData.dimensions,function(index,val){
 			var Qnum = 1;
-			// var dp = 1;
-			// console.log(dp);
-			// var create_page = document.createElement('div');
-			// create_page.setAttribute("id",'div'+dp);
-			// dp++;
-			$('#questions').append('<br><div class="alert alert-warning"><ul class="list-group"><strong>'+val.name+'</strong></ul></div>');
+			$('#page_divide').append('<div id="divide'+pd+'" name="'+pd+'"></div>');
+			$('#divide'+pd).append('<br><div class="alert alert-warning"><ul class="list-group"><strong>'+val.name+'</strong></ul></div>');
 			$.each(val.questions,function(qindex,qval){
-				// console.log(qval);
-				$('#questions').append('<li class="list-group-item"><p>'+(Qnum++)+'.'+qval.description+'</p></li>');
+				$('#divide'+pd).append('<li class="list-group-item"><p>'+(Qnum++)+'.'+qval.description+'</p></li>');
 				for (var i = 1; i <= ScaleData.level; i++) {
-					$('#questions').append(i+'<input type="radio" name="'+ScaleData.dimensions[1].name+'"id="'+ScaleData.dimensions[1].name+i+'">');
+					$('#divide'+pd).append(i+'<input type="radio" name="'+ScaleData.dimensions[1].name+'"id="'+ScaleData.dimensions[1].name+i+'">');
 				}
 			})
+			pd++;
 		})
+		$('div[id^="divide"]').hide();
 	})
 	function init(){
 		$.ajax({
@@ -32,10 +31,41 @@
 			}
 		})
 	}
+	function page_down(){
+		$('div[id^="divide"]').hide();
+		$('#divide'+i).show();
+		console.log(i);
+		if(i == ScaleData.dimensions.length){
+			$('#down').hide();
+		}else
+			i++;
+	}
+	function page_up(){
+	    --i;
+		$('div[id^="divide"]').hide();
+		$('#divide'+i).show();
+		console.log(i);
+		// if(i == 1){
+		// 	$('#up').hide();
+		// }
+	}
 </script>
 @endsection
 @section('content')
-<div id="questions" class="container"></div>
+<div id="page_divide">
+	<div id="questions" class="container">
+</div></div>
+<button id="up" onclick="page_up()"><img src="{{url('')}}/img/left-arrow.png"></button>
+<button id="down" onclick="page_down()"><img src="{{url('')}}/img/right-arrow.png"></button>
+<!-- <ul class="pagination">
+	<li><a href="#">&laquo;</a></li>
+	<li><a href="#">1</a></li>
+	<li><a href="#">2</a></li>
+	<li><a href="#">3</a></li>
+	<li><a href="#">4</a></li>
+	<li><a href="#">5</a></li>
+	<li><a href="#">&raquo;</a></li>
+</ul> -->
 @endsection
 @section('css')
 @endsection
